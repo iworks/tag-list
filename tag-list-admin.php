30c0566bf3ec3dad0d6df19ecc54350c428453bf
iworks <iworks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-16 20:26:16 +0000

    dodanie języka polskiego
    git-svn-id: https://plugins.svn.wordpress.org/tag-list/trunk@218465 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tag-list-admin.php
+++ tag-list-admin.php
@@ -20,8 +20,8 @@ class tag_list_admin
         add_submenu_page
         (
             'edit.php',
-            __('Tag list'),
-            __('Tag list'),
+            __('Tag list options', 'tag_list'),
+            __('Tag list options', 'tag_list'),
             9,
             basename(__FILE__),
             array( 'tag_list_admin', 'display_options' )
@@ -61,9 +61,9 @@ class tag_list_admin
         if ( !$matches[1] )
         {
             echo '<div id="message" class="error"><p><strong>';
-            _e('Warning');
+            _e('Warning', 'tag_list' );
             echo '</strong> ';
-            _e( 'wp_head(); not found in your head.php file, this might mean this plugin will not work!' );
+            _e( 'wp_head(); not found in your header.php file, this might mean this plugin will not work!', 'tag_list' );
             echo "</p></div>\n";
         }
         # Process updates, if any
@@ -83,7 +83,7 @@ class tag_list_admin
             . '</div>' . "\n";
         }
 
-        $options = iworks_tag_list_get_options();
+        $options = tag_list::get_options();
         # Display admin page
 ?>
 <div class="wrap">
@@ -96,74 +96,72 @@ class tag_list_admin
             <table class="form-table">
                 <tbody>
                     <tr>
-                        <th scope="row" valign="top"><label for="position"><?php _e('Use default CSS?'); ?></label></th>
+                        <th scope="row" valign="top"><label for="position"><?php _e('Use default CSS?', 'tag_list'); ?></label></th>
                         <td>
                             <ul>
-                                <li><input type="radio" name="tag_list[tag_list_default_css]" id="tag_list_default_css_on"<?php if ($options['tag_list_default_css'] == 'on' )  { echo ' checked="checked"';} ?> value="on"  /> <label for="tag_list_default_css_on"><?php _e('yes') ?></label></li>
-                                <li><input type="radio" name="tag_list[tag_list_default_css]" id="tag_list_default_css_on"<?php if ($options['tag_list_default_css'] == 'off' ) { echo ' checked="checked"';} ?> value="off" /> <label for="tag_list_default_css_on"><?php _e('no') ?></label></li>
+                                <li><input type="radio" name="tag_list[tag_list_default_css]" id="tag_list_default_css_on"<?php if ($options['tag_list_default_css'] == 'on' )  { echo ' checked="checked"';} ?> value="on"  /> <label for="tag_list_default_css_on"><?php _e('yes', 'tag_list') ?></label></li>
+                                <li><input type="radio" name="tag_list[tag_list_default_css]" id="tag_list_default_css_on"<?php if ($options['tag_list_default_css'] == 'off' ) { echo ' checked="checked"';} ?> value="off" /> <label for="tag_list_default_css_on"><?php _e('no', 'tag_list') ?></label></li>
                             </ul>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row" valign="top"><label for="position"><?php _e('Where should the short list be placed?'); ?></label></th>
+                        <th scope="row" valign="top"><label for="position"><?php _e('Where should the short list be placed?', 'tag_list'); ?></label></th>
                         <td>
                             <select name="tag_list[tag_list_position]" id="tag_list_position">
-                                <option value="both"<?php   if ($options['tag_list_position'] == 'both')   { echo ' selected="selected"';} ?>><?php _e('on top and on bottom (default)') ?></option>
-                                <option value="top"<?php    if ($options['tag_list_position'] == 'top')    { echo ' selected="selected"';} ?>><?php _e('only on top') ?></option>
-                                <option value="bottom"<?php if ($options['tag_list_position'] == 'bottom') { echo ' selected="selected"';} ?>><?php _e('only on bottom'); ?></option>
-                                <option value="none"<?php   if ($options['tag_list_position'] == 'hide')   { echo ' selected="selected"';} ?>><?php _e('hide') ?></option>
+                                <option value="both"<?php   if ($options['tag_list_position'] == 'both')   { echo ' selected="selected"';} ?>><?php _e('on top and on bottom (default)', 'tag_list') ?></option>
+                                <option value="top"<?php    if ($options['tag_list_position'] == 'top')    { echo ' selected="selected"';} ?>><?php _e('only on top', 'tag_list') ?></option>
+                                <option value="bottom"<?php if ($options['tag_list_position'] == 'bottom') { echo ' selected="selected"';} ?>><?php _e('only on bottom', 'tag_list'); ?></option>
+                                <option value="none"<?php   if ($options['tag_list_position'] == 'hide')   { echo ' selected="selected"';} ?>><?php _e('hide', 'tag_list') ?></option>
                             </select>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row" valign="top"><label for="position"><?php _e('Use extra div width ID?'); ?></label></th>
+                        <th scope="row" valign="top"><label for="position"><?php _e('Use extra div width ID?', 'tag_list'); ?></label></th>
                         <td>
                             <ul>
-                                <li><input type="radio" name="tag_list[tag_list_extra_div]" id="tag_list_extra_div_on"<?php if ($options['tag_list_extra_div'] == 'on' )  { echo ' checked="checked"';} ?> value="on"  /> <label for="tag_list_extra_div_on"><?php _e('yes') ?></label></li>
-                                <li><input type="radio" name="tag_list[tag_list_extra_div]" id="tag_list_extra_div_on"<?php if ($options['tag_list_extra_div'] == 'off' ) { echo ' checked="checked"';} ?> value="off" /> <label for="tag_list_extra_div_on"><?php _e('no') ?></label></li>
+                                <li><input type="radio" name="tag_list[tag_list_extra_div]" id="tag_list_extra_div_on"<?php if ($options['tag_list_extra_div'] == 'on' )  { echo ' checked="checked"';} ?> value="on"  /> <label for="tag_list_extra_div_on"><?php _e('yes', 'tag_list'); ?></label></li>
+                                <li><input type="radio" name="tag_list[tag_list_extra_div]" id="tag_list_extra_div_on"<?php if ($options['tag_list_extra_div'] == 'off' ) { echo ' checked="checked"';} ?> value="off" /> <label for="tag_list_extra_div_on"><?php _e('no', 'tag_list'); ?></label></li>
                             </ul>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row" valign="top"><label for="position"><?php _e('Show unused tag to?'); ?></label></th>
+                        <th scope="row" valign="top"><label for="position"><?php _e('Show unused tag to?', 'tag_list'); ?></label></th>
                         <td>
                             <ul>
-                                <li><input type="radio" name="tag_list[tag_list_unused_tags]" id="tag_list_unused_tags_on"<?php if ($options['tag_list_unused_tags'] == 'on' )  { echo ' checked="checked"';} ?> value="on"  /> <label for="tag_list_unused_tags_on"><?php _e('yes') ?></label></li>
-                                <li><input type="radio" name="tag_list[tag_list_unused_tags]" id="tag_list_unused_tags_on"<?php if ($options['tag_list_unused_tags'] == 'off' ) { echo ' checked="checked"';} ?> value="off" /> <label for="tag_list_unused_tags_on"><?php _e('no (default)') ?></label></li>
+                                <li><input type="radio" name="tag_list[tag_list_unused_tags]" id="tag_list_unused_tags_on"<?php if ($options['tag_list_unused_tags'] == 'on' )  { echo ' checked="checked"';} ?> value="on"  /> <label for="tag_list_unused_tags_on"><?php _e('yes', 'tag_list'); ?></label></li>
+                                <li><input type="radio" name="tag_list[tag_list_unused_tags]" id="tag_list_unused_tags_on"<?php if ($options['tag_list_unused_tags'] == 'off' ) { echo ' checked="checked"';} ?> value="off" /> <label for="tag_list_unused_tags_on"><?php _e('no (default)', 'tag_list'); ?></label></li>
                             </ul>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row" valign="top"><label for="position"><?php _e('Show number of use?'); ?></label></th>
+                        <th scope="row" valign="top"><label for="position"><?php _e('Show number of use?', 'tag_list'); ?></label></th>
                         <td>
                             <ul>
-                                <li><input type="radio" name="tag_list[tag_list_number_of_use]" id="tag_list_number_of_use_on"<?php if ($options['tag_list_number_of_use'] == 'on' )  { echo ' checked="checked"';} ?> value="on"  /> <label for="tag_list_number_of_use_on"><?php _e('yes') ?></label></li>
-                                <li><input type="radio" name="tag_list[tag_list_number_of_use]" id="tag_list_number_of_use_on"<?php if ($options['tag_list_number_of_use'] == 'off' ) { echo ' checked="checked"';} ?> value="off" /> <label for="tag_list_number_of_use_on"><?php _e('no (default)') ?></label></li>
+                                <li><input type="radio" name="tag_list[tag_list_number_of_use]" id="tag_list_number_of_use_on"<?php if ($options['tag_list_number_of_use'] == 'on' )  { echo ' checked="checked"';} ?> value="on"  /> <label for="tag_list_number_of_use_on"><?php _e('yes', 'tag_list') ?></label></li>
+                                <li><input type="radio" name="tag_list[tag_list_number_of_use]" id="tag_list_number_of_use_on"<?php if ($options['tag_list_number_of_use'] == 'off' ) { echo ' checked="checked"';} ?> value="off" /> <label for="tag_list_number_of_use_on"><?php _e('no (default)', 'tag_list') ?></label></li>
                             </ul>
                         </td>
                     </tr>
                 </tbody>
             </table>
         </fieldset>
-        <p class="submit"><input type="submit" value="<?php _e('Save Changes', 'tag_list') ?>" /></p>
+        <p class="submit"><input type="submit" value="<?php _e('Save Changes', 'tag_list', 'tag_list'); ?>" /></p>
     </form>
-    <?php /*
-    <h2><?php _e('tag_list Options', 'tag_list') ?></h2>
+    <h2><?php _e('Donation', 'tag_list') ?></h2>
     <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
         <input type="hidden" name="cmd" value="_donations">
         <input type="hidden" name="business" value="marcin@iworks.pl">
-        <input type="hidden" name="item_name" value="tag_list plugin">
+        <input type="hidden" name="item_name" value="tag list plugin">
         <input type="hidden" name="no_shipping" value="0">
         <input type="hidden" name="no_note" value="1">
         <input type="hidden" name="currency_code" value="PLN">
         <input type="hidden" name="tax" value="0">
         <input type="hidden" name="lc" value="PL">
         <input type="hidden" name="bn" value="PP-DonationsBF">
-        <input type="image" src="https://www.paypal.com/pl_PL/PL/i/btn/btn_donate_LG.gif" border="0"
+        <input type="image" src="https://www.paypalobjects.com/WEBSCR-620-20100302-1/en_US/i/logo/PayPal_mark_60x38.gif" border="0"
         name="submit" alt="PayPal - Wygodne i bezpieczne płatności internetowe!">
         <img alt="" border="0" src="https://www.paypal.com/pl_PL/i/scr/pixel.gif" width="1" height="1">
     </form>
-    */ ?>
 </div>
 <?php
     } # display_options()
--- tag-list.php
+++ tag-list.php
@@ -33,123 +33,157 @@ if ( strpos($_SERVER['REQUEST_URI'], 'wp-admin') !== false )
     include_once dirname(__FILE__) . '/tag-list-admin.php';
 }
 
-load_plugin_textdomain('tag_list');
-add_shortcode( 'tag-list',  'get_iworks_tag_list' );
-add_action('wp_head', 'get_iworks_tag_list_css');
 
-#
-# get_options()
-#
-
-function iworks_tag_list_get_options()
+class tag_list
 {
-    if ( function_exists('get_site_option') )
-    {
-        $options = get_site_option( 'tag_list_params' );
-    }
-    else
-    {
-        $options = get_option( 'tag_list_params' );
-    }
-    if ( !isset($options['tag_list_position'] ) )
-    {
-        $options['tag_list_position'] = 'both';
-    }
-    if ( !isset($options['tag_list_default_css'] ) )
-    {
-        $options['tag_list_default_css'] = 'on';
-    }
-    if ( !isset($options['tag_list_extra_div'] ) )
-    {
-        $options['tag_list_extra_div'] = 'on';
-    }
-    if ( !isset($options['tag_list_unused_tags'] ) )
-    {
-        $options['tag_list_unused_tags'] = 'off';
-    }
-    if ( !isset($options['tag_list_number_of_use'] ) )
-    {
-        $options['tag_list_number_of_use'] = 'off';
-    }
-    return $options;
-} # get_options()
-
-#
-# get_iworks_tag_list()
-#
-function get_iworks_tag_list()
-{
-    global $wpdb;
     #
-    $options = iworks_tag_list_get_options();
+    # init()
     #
-    $sql  = 'SELECT T.term_id, T.name, X.count ';
-    $sql .= 'FROM ' . $wpdb->terms . ' T ';
-    $sql .= 'LEFT JOIN ' . $wpdb->term_taxonomy . ' X ON T.term_id = X.term_id ';
-    $sql .= 'WHERE X.taxonomy = \'post_tag\' ';
-    if ( $options['tag_list_unused_tags'] == 'off' )
+    function init()
     {
-        $sql .= 'AND X.count > 0 ';
-    }
-    $sql .= 'ORDER BY T.name ASC';
-    $tags = $wpdb->get_results( $sql );
-    #
-    $letter = '';
-    $toc = '<ul class="tag-toc">';
-    $content = '<ul class="tag-list">';
-    $count = 0;
-    foreach ($tags as $t)
-    {
-        $l = mb_substr ( $t->name, 0, 1 );
-        if (strtolower($l) != $letter)
-        {
-            if ($letter != '')
+        add_shortcode( 'tag-list',  Array( 'tag_list', 'get_tag_list' ) );
+        add_action( 'wp_head', Array( 'tag_list', 'get_css') );
+        add_filter( 'plugin_row_meta', Array( 'tag_list', 'register_plugin_links' ), 10, 2 );
+        // load language file
+        $current_locale = get_locale();
+        if(!empty($current_locale)) {
+            $mo_file = dirname(__FILE__) . "/lang/tag-list-" . $current_locale . ".mo";
+            if( @file_exists( $mo_file ) && is_readable( $mo_file ) )
             {
-                $content .= '</ul>'."\n".'</li>'."\n";
+                load_textdomain('tag_list', $mo_file);
             }
-            $letter = strtolower($l);
-            $archor = 'tag-'.((preg_match('/[a-z0-9]/', $letter))? $letter:'alt-'.$count);
-            $toc .= sprintf('<li><a href="#%s">%s</a></li>', $archor, $letter);
-            $content .= sprintf('<li id="%s">'."\n".'<h4>%s</h4>'."\n".'<ul>'."\n", $archor, $letter);
         }
-        $link = get_tag_link($t->term_id);
-        if ( is_wp_error( $link ) )
-        {
-            return $link;
-        }
-        $counter_string = '';
-        if ( $options['tag_list_number_of_use'] == 'on' )
-        {
-            $counter_string = sprintf( ' <small>(%d)</small>', $t->count );
-        }
-        $content .= sprintf('<li><a href="%s">%s%s</a></li>'."\n", $link, $t->name, $counter_string);
-        $count++;
-    }
-    $content .= '</ul>'."\n".'</li>'."\n".'</ul>'."\n";
-    $toc .= '</ul>'."\n";
-    if ( preg_match( '/^(top|both)$/', $options['tag_list_position'] ) ) {
-        $content = $toc.$content;
-    }
-    if ( preg_match( '/^(bottom|both)$/', $options['tag_list_position'] ) ) {
-        $content .= $toc;
-    }
-    if ( $options['tag_list_extra_div'] == 'on' )
+    } # init()
+
+
+    #
+    # get_options()
+    #
+    function get_options()
     {
-        return '<div id="tag-list">'.$content.'</div>';
-    }
-    return $content;
-}#get_iworks_tag_list()
-#
-# get_iworks_tag_list_css()
-#
-function get_iworks_tag_list_css()
-{
-    $options = iworks_tag_list_get_options();
-    if ( $options['tag_list_default_css'] == 'on' )
+        if ( function_exists('get_site_option') )
+        {
+            $options = get_site_option( 'tag_list_params' );
+        }
+        else
+        {
+            $options = get_option( 'tag_list_params' );
+        }
+        if ( !isset($options['tag_list_position'] ) )
+        {
+            $options['tag_list_position'] = 'both';
+        }
+        if ( !isset($options['tag_list_default_css'] ) )
+        {
+            $options['tag_list_default_css'] = 'on';
+        }
+        if ( !isset($options['tag_list_extra_div'] ) )
+        {
+            $options['tag_list_extra_div'] = 'on';
+        }
+        if ( !isset($options['tag_list_unused_tags'] ) )
+        {
+            $options['tag_list_unused_tags'] = 'off';
+        }
+        if ( !isset($options['tag_list_number_of_use'] ) )
+        {
+            $options['tag_list_number_of_use'] = 'off';
+        }
+        return $options;
+    } # get_options()
+
+    #
+    # get_iworks_tag_list()
+    #
+    function get_tag_list()
     {
-        print '<style type="text/css" >';
-        include_once('default.css');
-        print '</style>';
-    }
+        global $wpdb;
+        #
+        $options = tag_list::get_options();
+        #
+        $sql  = 'SELECT T.term_id, T.name, X.count ';
+        $sql .= 'FROM ' . $wpdb->terms . ' T ';
+        $sql .= 'LEFT JOIN ' . $wpdb->term_taxonomy . ' X ON T.term_id = X.term_id ';
+        $sql .= 'WHERE X.taxonomy = \'post_tag\' ';
+        if ( $options['tag_list_unused_tags'] == 'off' )
+        {
+            $sql .= 'AND X.count > 0 ';
+        }
+        $sql .= 'ORDER BY T.name ASC';
+        $tags = $wpdb->get_results( $sql );
+        #
+        $letter = '';
+        $toc = '<ul class="tag-toc">';
+        $content = '<ul class="tag-list">';
+        $count = 0;
+        foreach ($tags as $t)
+        {
+            $l = mb_substr ( $t->name, 0, 1 );
+            if (strtolower($l) != $letter)
+            {
+                if ($letter != '')
+                {
+                    $content .= '</ul>'."\n".'</li>'."\n";
+                }
+                $letter = strtolower($l);
+                $archor = 'tag-'.((preg_match('/[a-z0-9]/', $letter))? $letter:'alt-'.$count);
+                $toc .= sprintf('<li><a href="#%s">%s</a></li>', $archor, $letter);
+                $content .= sprintf('<li id="%s">'."\n".'<h4>%s</h4>'."\n".'<ul>'."\n", $archor, $letter);
+            }
+            $link = get_tag_link($t->term_id);
+            if ( is_wp_error( $link ) )
+            {
+                return $link;
+            }
+            $counter_string = '';
+            if ( $options['tag_list_number_of_use'] == 'on' )
+            {
+                $counter_string = sprintf( ' <small>(%d)</small>', $t->count );
+            }
+            $content .= sprintf('<li><a href="%s">%s%s</a></li>'."\n", $link, $t->name, $counter_string);
+            $count++;
+        }
+        $content .= '</ul>'."\n".'</li>'."\n".'</ul>'."\n";
+        $toc .= '</ul>'."\n";
+        if ( preg_match( '/^(top|both)$/', $options['tag_list_position'] ) ) {
+            $content = $toc.$content;
+        }
+        if ( preg_match( '/^(bottom|both)$/', $options['tag_list_position'] ) ) {
+            $content .= $toc;
+        }
+        if ( $options['tag_list_extra_div'] == 'on' )
+        {
+            return '<div id="tag-list">'.$content.'</div>';
+        }
+        return $content;
+    }#get_iworks_tag_list()
+
+    #
+    # get_css()
+    #
+    function get_css()
+    {
+        $options = tag_list::get_options();
+        if ( $options['tag_list_default_css'] == 'on' )
+        {
+            print '<style type="text/css" >';
+            include_once('default.css');
+            print '</style>';
+        }
+    } # get_css()
+
+    #
+    # register_plugin_links()
+    #
+    function register_plugin_links($links, $file)
+    {
+        if ( preg_match( '/tag-list.php$/', $file ) )
+        {
+            $links[] = '<a href="edit.php?page=tag-list-admin.php">' . __( 'Settings', 'tag_list' ) . '</a>';
+        }
+        return $links;
+    }# register_plugin_links()
 }
+
+tag_list::init();
 ?>
